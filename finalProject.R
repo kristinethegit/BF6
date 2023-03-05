fbb8ce851e6b1e53ce5e5b30b8de023031e46b2b
simran <121985223+simmi33@users.noreply.github.com>
2023-03-05 00:38:58 -0800

    Add files via upload

--- finalProject.R
+++ finalProject.R
@@ -8,55 +8,41 @@
 #
 
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+#loading data
+#OregonFires <- read.delim()
+  
+#Title   
+titlePanel("Wildfires in Oregon")
+  
+  
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Oregon Wildfires"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          tabsetPanel(
-            tabPanel("Overview", plotOutput("plot"), 
-                     tabPanelBody("panel2", "Panel 2 content")),
-            tabPanel("Table", tableOutput("table"), 
-                     tabPanelBody("panel3", "Panel 3 content")),
-            tabPanel("Page", plotOutput("plot"),
-                     tabPanelBody("panel4", "Panel 4 conent")),
-            tabPanel("Page", plotOutput("plot"),
-                     tabPanelBody("panel5", "Panel 5 conent")),
-            tabPanel("Conclusion", textOutput("Conclusion"), 
-                     tabPanelBody("panel1", "Panel 1 content"))
-          )
-        )
+  tabsetPanel(
+    tabPanel("Overview",
+             sidebarPanel(
+               # Sidebar content for Tab 1
+               
+             ),
+             mainPanel(
+               # Main content for Tab 1
+             )
+    ),
+    tabPanel("Tab 2",
+             sidebarPanel(
+               # Sidebar content for Tab 2
+             ),
+             mainPanel(
+               # Main content for Tab 2
+             )
     )
+  )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+#server
+server <- function(input, output){
+  
+  
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
