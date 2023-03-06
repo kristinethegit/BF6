2f526f9a6da85da51117544ea0f757e5f17d9952
simran <121985223+simmi33@users.noreply.github.com>
2023-03-05 22:03:14 -0800

    updated the tabs and added info in overview sect

--- finalProject.R
+++ finalProject.R
@@ -11,32 +11,58 @@ library(shiny)
 library(tidyverse)
 
 #loading data
+
 #OregonFires <- read.delim()
   
-#Title   
-titlePanel("Wildfires in Oregon")
+
   
   
 ui <- fluidPage(
+  
+  #Title   
+  titlePanel("Wildfires in Oregon"),
+  
+  # tabs
   tabsetPanel(
+    #project overview tab
     tabPanel("Overview",
-             sidebarPanel(
-               # Sidebar content for Tab 1
-               
-             ),
              mainPanel(
-               # Main content for Tab 1
+            h2(strong("Project Overview")),
+            p("This report will provide a broad summary of wildfires in the state of Oregon and its causes."),
+            h2(strong("Data Set")),
+            p("This app uses wild fire data from DATA.GOV. The data set from the Oregon Department of Forestry (ODF) and it spans from 2000 through 2022."),
+            a("Access link here", href='https://catalog.data.gov/dataset/odf-fire-occurrence-data-2000-2022'),
+            
              )
     ),
-    tabPanel("Tab 2",
+    #causes of wildfires tab
+    tabPanel("Causes of wildfires in Oregon",
              sidebarPanel(
                # Sidebar content for Tab 2
              ),
              mainPanel(
                # Main content for Tab 2
              )
-    )
-  )
+    ),
+    #human fires tab
+    tabPanel("How frequent are human induced fires?",
+              sidebarPanel(
+                # Sidebar content for Tab 3
+             ),
+               mainPanel(
+                   # Main content for Tab 3
+             )
+  ),
+  #early vs later fires
+  tabPanel("How have wildfires in earlier years differed from later years?",
+           sidebarPanel(
+             # Sidebar content for Tab 3
+           ),
+           mainPanel(
+             # Main content for Tab 3
+          )
+)
+)
 )
 #server
 server <- function(input, output){
